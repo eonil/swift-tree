3d0c8c2fa62902d010b9400a286b0dba01807550
eonil <pub.dev@eonil.com>
2019-06-27 19:27:18 +0900

    Add KVLTStorage.List.keys.

---
+++ Tree/KVLTStorage.List.keys.swift
@@ -0,0 +1,29 @@
+//
+//  KVLTStorage.List.keys.swift
+//  Tree
+//
+//  Created by Henry on 2019/06/27.
+//  Copyright © 2019 Eonil. All rights reserved.
+//
+
+public extension KVLTStorage.List {
+    var keys: Keys {
+        return Keys(base: self)
+    }
+    struct Keys: RandomAccessCollection {
+        private(set) var base: KVLTStorage.List
+    }
+}
+public extension KVLTStorage.List.Keys {
+    typealias Index = Int
+    typealias Element = Key
+    var startIndex: Index {
+        return base.startIndex
+    }
+    var endIndex: Index {
+        return base.endIndex
+    }
+    subscript(_ i: Index) -> Key {
+        return base[i].key
+    }
+}
