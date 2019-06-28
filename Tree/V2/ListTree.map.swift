3bf11bb76d8cc632f7b98a9dba8cadb925b8172c
eonil <pub.dev@eonil.com>
2019-06-28 22:01:26 +0900

    Add preferred overload.

--- Tree/V2/ListTree.map.swift
+++ Tree/V2/ListTree.map.swift
@@ -8,6 +8,17 @@
 
 public extension ListTree {
     /// Makes a new `T` instance with mapped values.
+    /// This overload makes Swift compiler to prefer `ListTree`
+    /// type if returning type is not been specified.
+    func map<X>(_ fx: (Value) -> X) -> ListTree<X> {
+        var t1 = ListTree<X>(value: fx(value))
+        for ct in collection {
+            let ct1 = ct.map(fx)
+            t1.collection.append(ct1)
+        }
+        return t1
+    }
+    /// Makes a new `T` instance with mapped values.
     func map<T,X>(_ fx: (Value) -> X) -> T where
     T: ReplaceableListTreeProtocol,
     T.Value == X {
